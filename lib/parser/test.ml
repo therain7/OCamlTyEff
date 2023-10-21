51a0b28c09eee082795422751d68035018588635
Andrei <therain.i@yahoo.com>
2023-10-21 17:09:08 +0300

    test(parser): add factorial test

---
+++ lib/parser/test.ml
@@ -0,0 +1,37 @@
+open! Base
+open Ast
+
+(** Parse string and pretty print the output *)
+let pp str = Stdlib.Format.printf "%a" pp_structure (Parser.parse_exn str)
+
+let%expect_test "parse_fact" =
+  pp "let rec fact n = if n < 2 then 1 else n * fact (n - 1)" ;
+  [%expect
+    {|
+    [(Str_value (Recursive,
+        [{ pat = (Pat_var "fact");
+           expr =
+           (Exp_function ([(Pat_var "n")],
+              (Function_body
+                 (Exp_ifthenelse (
+                    (Exp_apply (
+                       (Exp_apply ((Exp_ident (Ident "<")),
+                          (Exp_ident (Ident "n")))),
+                       (Exp_constant (Const_integer 2)))),
+                    (Exp_constant (Const_integer 1)),
+                    (Some (Exp_apply (
+                             (Exp_apply ((Exp_ident (Ident "*")),
+                                (Exp_ident (Ident "n")))),
+                             (Exp_apply ((Exp_ident (Ident "fact")),
+                                (Exp_apply (
+                                   (Exp_apply ((Exp_ident (Ident "-")),
+                                      (Exp_ident (Ident "n")))),
+                                   (Exp_constant (Const_integer 1))))
+                                ))
+                             )))
+                    )))
+              ))
+           }
+          ]
+        ))
+      ] |}]
