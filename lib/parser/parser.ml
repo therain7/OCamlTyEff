6bd205d549fb3c068a4afc6f7b96ed302bff2ea8
Andrei <therain.i@yahoo.com>
2023-10-21 06:51:58 +0300

    refactor(parser): change `parse` signature to return string on error

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,11 +1,5 @@
 open! Base
 
 let parse str =
-  match
-    Angstrom.parse_string Structure.parse_structure
-      ~consume:Angstrom.Consume.All str
-  with
-  | Result.Ok x ->
-      Result.Ok x
-  | Error err ->
-      Result.Error (`ParsingError err)
+  Angstrom.parse_string ~consume:Angstrom.Consume.All Structure.parse_structure
+    str
