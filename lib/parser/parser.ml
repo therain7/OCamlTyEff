b44a0353f9136d7be4a7dec64399985f84f91724
Andrei <therain.i@yahoo.com>
2023-10-21 17:08:01 +0300

    feat(parser): add `parse_exn`

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -3,3 +3,5 @@ open! Base
 let parse str =
   Angstrom.parse_string ~consume:Angstrom.Consume.All Structure.parse_structure
     str
+
+let parse_exn str = Result.ok_or_failwith (parse str)
--- lib/parser/parser.mli
+++ lib/parser/parser.mli
@@ -1,3 +1,6 @@
 open! Base
 
 val parse : string -> (Ast.structure, string) result
+
+val parse_exn : string -> Ast.structure
+(** Raises [Failure] exception if parsing fails *)
