105f3a69ddc310343a3ccc2f00510da24a9d4888
Danil S <danil.slinchuk@gmail.com>
2023-10-21 20:43:35 +0300

    test(parser): add `parse_rec_flag` with `parse_expression` test

--- lib/parser/test.ml
+++ lib/parser/test.ml
@@ -4,6 +4,24 @@ open Ast
 (** Parse string and pretty print the output *)
 let pp str = Stdlib.Format.printf "%a" pp_structure (Parser.parse_exn str)
 
+let%expect_test "parse_let_rec_without_whitespaces" =
+  pp "letrec f n = n + 1" ;
+  [%expect
+    {|
+    [(Str_eval
+        (Exp_apply (
+           (Exp_apply ((Exp_ident (Ident "=")),
+              (Exp_apply (
+                 (Exp_apply ((Exp_ident (Ident "letrec")),
+                    (Exp_ident (Ident "f")))),
+                 (Exp_ident (Ident "n"))))
+              )),
+           (Exp_apply (
+              (Exp_apply ((Exp_ident (Ident "+")), (Exp_ident (Ident "n")))),
+              (Exp_constant (Const_integer 1))))
+           )))
+      ] |}]
+
 let%expect_test "parse_fact" =
   pp "let rec fact n = if n < 2 then 1 else n * fact (n - 1)" ;
   [%expect
