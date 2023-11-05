d44940cba738ab28d65d173afbf0a83aaaffa0a2
Danil S <danil.slinchuk@gmail.com>
2023-11-05 22:30:37 +0300

    test(parser): move all unit tests to unit_tests folder

---
+++ lib/parser/unit_tests/commont.ml
@@ -0,0 +1,67 @@
+(** Copyright 2023, Danil S, Andrei *)
+
+(** SPDX-License-Identifier: MIT *)
+
+open! Base
+open Ast
+open Common
+
+let%expect_test "parse_pat_bool1" =
+  pp Stdlib.Format.pp_print_string parse_constr_name "true" ;
+  [%expect {| true |}]
+
+let%expect_test "parse_pat_bool2" =
+  pp Stdlib.Format.pp_print_string parse_constr_name "false" ;
+  [%expect {| false |}]
+
+let%expect_test "parse_pat_unit" =
+  pp Stdlib.Format.pp_print_string parse_constr_name "()" ;
+  [%expect {| () |}]
+
+let%expect_test "parse_quoted_string_literal1" =
+  pp pp_constant parse_const "{|Hello world!|}" ;
+  [%expect {| (Const_string "Hello world!") |}]
+
+let%expect_test "parse_quoted_string_literal2" =
+  pp pp_constant parse_const "{aa|Hello world!|aa}" ;
+  [%expect {| (Const_string "Hello world!") |}]
+
+let%expect_test "parse_quoted_string_literal3" =
+  pp pp_constant parse_const "{aa|Hello |aa world!|aa}" ;
+  [%expect {| (Const_string "Hello |aa world!") |}]
+
+let%expect_test "parse_value_name1" =
+  pp Stdlib.Format.pp_print_string parse_value_name "abc" ;
+  [%expect {| abc |}]
+
+let%expect_test "parse_value_name2" =
+  pp Stdlib.Format.pp_print_string parse_value_name "a0b'c_d" ;
+  [%expect {| a0b'c_d |}]
+
+let%expect_test "parse_value_name3" =
+  pp Stdlib.Format.pp_print_string parse_value_name "_0a" ;
+  [%expect {| _0a |}]
+
+let%expect_test "parse_value_name_operator1" =
+  pp Stdlib.Format.pp_print_string parse_value_name "(>>)" ;
+  [%expect {| >> |}]
+
+let%expect_test "parse_value_name_operator2" =
+  pp Stdlib.Format.pp_print_string parse_value_name "(%>)" ;
+  [%expect {| %> |}]
+
+let%expect_test "parse_value_name_operator3" =
+  pp Stdlib.Format.pp_print_string parse_value_name "(!)" ;
+  [%expect {| ! |}]
+
+let%expect_test "parse_value_name_operator4" =
+  pp Stdlib.Format.pp_print_string parse_value_name "(~:=)" ;
+  [%expect {| ~:= |}]
+
+let%expect_test "parse_value_name_operator5" =
+  pp Stdlib.Format.pp_print_string parse_value_name "(@<>)" ;
+  [%expect {| @<> |}]
+
+let%expect_test "parse_constr_name" =
+  pp Stdlib.Format.pp_print_string parse_constr_name "Nil" ;
+  [%expect {| Nil |}]
