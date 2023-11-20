47a437aa618ba87829bbd67bc2332e616159acce
Andrei <therain.i@yahoo.com>
2023-11-20 03:38:29 +0300

    refactor(unit_tests): remove module prefix from tests name

--- lib/parser/unit_tests/typt.ml
+++ lib/parser/unit_tests/typt.ml
@@ -7,35 +7,35 @@ open Ast
 open Common
 open Typ
 
-let%expect_test "parse_typ_var" =
+let%expect_test "parse_var" =
   pp pp_typ parse_typ "'some_type_var" ;
   [%expect {| (Typ_var "some_type_var") |}]
 
-let%expect_test "parse_typ_arrow" =
+let%expect_test "parse_arrow" =
   pp pp_typ parse_typ "'a -> 'b -> 'c" ;
   [%expect
     {| (Typ_arrow ((Typ_var "a"), (Typ_arrow ((Typ_var "b"), (Typ_var "c"))))) |}]
 
-let%expect_test "parse_typ_tuple" =
+let%expect_test "parse_tuple" =
   pp pp_typ parse_typ "'a * 'b * 'c" ;
   [%expect {| (Typ_tuple [(Typ_var "a"); (Typ_var "b"); (Typ_var "c")]) |}]
 
-let%expect_test "parse_typ_constr1" =
+let%expect_test "parse_constr1" =
   pp pp_typ parse_typ "int" ; [%expect {| (Typ_constr ((Ident "int"), [])) |}]
 
-let%expect_test "parse_typ_constr2" =
+let%expect_test "parse_constr2" =
   pp pp_typ parse_typ "int list" ;
   [%expect
     {| (Typ_constr ((Ident "list"), [(Typ_constr ((Ident "int"), []))])) |}]
 
-let%expect_test "parse_typ_constr3" =
+let%expect_test "parse_constr3" =
   pp pp_typ parse_typ "(int, string) map" ;
   [%expect
     {|
     (Typ_constr ((Ident "map"),
        [(Typ_constr ((Ident "int"), [])); (Typ_constr ((Ident "string"), []))])) |}]
 
-let%expect_test "parse_typ_constr4" =
+let%expect_test "parse_constr4" =
   pp pp_typ parse_typ "('a -> int * (string, unit, 'b -> 'c) foo bar) -> e" ;
   [%expect
     {|
