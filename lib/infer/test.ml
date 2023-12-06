a3d5d153bae2c94f8cde6f77fb347e71a1e69970
Andrei <therain.i@yahoo.com>
2023-12-06 06:07:59 +0300

    test(infer): add simple tests for type inference

---
+++ lib/infer/test.ml
@@ -0,0 +1,31 @@
+open! Base
+open Types
+
+let run code =
+  match Parse.parse code with
+  | Some [str_item] -> (
+    match Infer.infer_structure_item StdEnv.env str_item with
+    | Ok (ty, _) ->
+        Scheme.pp Stdlib.Format.std_formatter ty
+    | Error err ->
+        Infer.TyError.pp Stdlib.Format.std_formatter err )
+  | None ->
+      Stdlib.print_endline "syntax error"
+  | _ ->
+      Stdlib.print_endline "invalid test"
+
+let%expect_test "infer_id" =
+  run {| fun x -> x |} ;
+  [%expect
+    {|
+    (Forall ({(Var "gen1")},
+       (Ty_arr ((Ty_var (Var "gen1")), (Ty_var (Var "gen1")))))) |}]
+
+let%expect_test "infer_add" =
+  run {| fun x y -> x + y |} ;
+  [%expect
+    {|
+    (Forall ({},
+       (Ty_arr ((Ty_con ((Ident "int"), [])),
+          (Ty_arr ((Ty_con ((Ident "int"), [])), (Ty_con ((Ident "int"), []))))))
+       )) |}]
