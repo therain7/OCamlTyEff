f7be6504a489ff85e740e4c3ff288efa85391b64
Andrei <therain.i@yahoo.com>
2023-12-10 06:15:05 +0300

    feat(inter): add lists to std type environment

--- lib/types/stdEnv.ml
+++ lib/types/stdEnv.ml
@@ -7,24 +7,30 @@ let var x = Ty.Ty_var (Var x)
 
 let ( @> ) ty_arg ty_res = Ty.Ty_arr (ty_arg, ty_res)
 
+let tuple tys = Ty.Ty_tuple tys
+
 let no_vars ty = Scheme.Forall (VarSet.empty, ty)
 
-let basic_arith = Ty.int @> Ty.int @> Ty.int
+let single_var ty = Scheme.Forall (VarSet.singleton @@ Var "a", ty)
+let alpha = var "a"
+
+let arith = Ty.int @> Ty.int @> Ty.int
+
+let alpha_option = Ty.Ty_con (id "option", [alpha])
+
+let alpha_list = Ty.Ty_con (id "list", [alpha])
 
 let env =
-  Env.of_alist_exn
-    [ (id "id", Forall (VarSet.singleton @@ Var "a", var "a" @> var "a"))
-    ; (id "+", no_vars basic_arith)
-    ; (id "-", no_vars basic_arith)
-    ; (id "*", no_vars basic_arith)
-    ; (id "=", no_vars (Ty.int @> Ty.int @> Ty.bool))
-    ; (id "true", no_vars Ty.bool)
-    ; (id "false", no_vars Ty.bool)
-    ; (id "()", no_vars Ty.unit)
-    ; ( id "Some"
-      , Forall
-          ( VarSet.singleton @@ Var "a"
-          , var "a" @> Ty_con (Ident "option", [var "a"]) ) )
-    ; ( id "None"
-      , Forall (VarSet.singleton @@ Var "a", Ty_con (Ident "option", [var "a"]))
-      ) ]
+  [ (id "id", single_var (alpha @> alpha))
+  ; (id "+", no_vars arith)
+  ; (id "-", no_vars arith)
+  ; (id "*", no_vars arith)
+  ; (id "=", no_vars (Ty.int @> Ty.int @> Ty.bool))
+  ; (id "true", no_vars Ty.bool)
+  ; (id "false", no_vars Ty.bool)
+  ; (id "()", no_vars Ty.unit)
+  ; (id "Some", single_var (alpha @> alpha_option))
+  ; (id "None", single_var alpha_option)
+  ; (id "[]", single_var alpha_list)
+  ; (id "::", single_var (tuple [alpha; alpha_list] @> alpha_list)) ]
+  |> Env.of_alist_exn
