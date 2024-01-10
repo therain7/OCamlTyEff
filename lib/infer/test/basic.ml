0cd4331583fb385cb066dfc0b14d343c655d3421
Andrei <therain.i@yahoo.com>
2024-01-10 05:16:27 +0300

    test(infer): restructure

--- lib/infer/test/basic.ml
+++ lib/infer/test/basic.ml
@@ -3,26 +3,7 @@
 (** SPDX-License-Identifier: MIT *)
 
 open! Base
-open Types
-
-let run code =
-  let open Stdlib.Format in
-  match Parse.parse code with
-  | Some [str_item] -> (
-    match Infer.infer_structure_item StdEnv.env str_item with
-    | Ok (env, bounds, sc) ->
-        printf "%a\n" Scheme.pp sc ;
-
-        List.iter bounds ~f:(fun bound_var ->
-            let sc = Env.find_exn env bound_var in
-            let (Ident name) = bound_var in
-            printf "%s: %a\n" name Scheme.pp sc )
-    | Error err ->
-        Infer.TyError.pp Stdlib.Format.std_formatter err )
-  | None ->
-      Stdlib.print_endline "syntax error"
-  | _ ->
-      Stdlib.print_endline "invalid test"
+open Run
 
 let%expect_test _ =
   run {|
