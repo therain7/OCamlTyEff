0c65fe5d5fd7ebe26cdfaa29b6c43f0c3d112639
Andrei <therain.i@yahoo.com>
2024-02-22 03:06:35 +0300

    feat: add list functions to prelude

--- lib/builtin/prelude.ml
+++ lib/builtin/prelude.ml
@@ -17,4 +17,25 @@ let fst (x, y) = x;;
 let snd (x, y) = y;;
 
 let not x = if x then false else true;;
+
+let list_map f l =
+  let rec helper acc = function
+    | [] -> acc []
+    | x :: xs -> helper (fun ys -> acc (f x :: ys)) xs
+  in
+  helper (fun ys -> ys) l
+;;
+
+let list_rev l =
+  let rec helper acc = function
+    | [] -> acc
+    | hd :: tl -> helper (hd :: acc) tl
+  in
+  helper [] l
+;;
+
+let rec list_iter f = function
+  | [] -> ()
+  | hd :: tl -> let () = f hd in list_iter f tl
+;;
 |}
